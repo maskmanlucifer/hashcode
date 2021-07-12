016db51e2cd47219ed54d442c90a6151f5627d4f
Aman Kumar Singh <maskmanaman@gmail.com>
2021-07-12 10:59:23 +0530

    🍔 navbar added

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 
+let user;
+
 app.get('/',(req,res)=>{
    res.render('home');
+});
+
+app.get('/snippet',(req,res)=>{
+  res.render('snippet');
+});
+
+app.get('/contest',(req,res)=>{
+  res.render('contest');
 });
 
 app.listen(3000,()=>{
