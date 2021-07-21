e0512296e232d5e50592f310eb3978aef6990052
Aman <maskmanaman@gmail.com>
2021-07-21 11:49:00 +0530

    improving visualizer page

--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -1,4 +1,3 @@
-
 let contest_id = Number(document.getElementById('contest_id').textContent);
 let findLevel = (rating) =>{
     if(rating < 1200) {
@@ -54,34 +53,34 @@ let dataExtract =  async (contestID)=>{
       throw new Error('Contest not exist');
   }
   let data = await standing.json();
-
+  
   if(data.result.contest.phase !== "FINISHED") {
     throw new Error('Contest is ongoing');
   }
-
-  let startTime = data.result.contest.startTimeSeconds;
-  let duration = data.result.contest.durationSeconds;
   
   let url1 = "https://codeforces.com/api/contest.ratingChanges?contestId=";
   url1 += contestID;
 
   let ratingChange = await fetch(url1);
-
-  let data1 = await ratingChange.json();
-
-  let handleRating = {};
   
-  for(let i=data1.result.length-1;i>=0;i--) {
-      
-    let oldRating = data1.result[i].oldRating;
-    if(oldRating >=0 ) {
+  let data1 = await ratingChange.json();
+  
+  
+  let handleRating = {};
+    
+    
+    for(let i=data1.result.length-1;i>=0;i--) {
+        
+        let oldRating = data1.result[i].oldRating;
+        if(oldRating >=0 ) {
 
-    } else {
-        oldRating = 0;
+        } else {
+            oldRating = 0;
+        }
+        handleRating[data1.result[i].handle]= findLevel(oldRating);
     }
-    handleRating[data1.result[i].handle]= findLevel(oldRating);
-  }
 
+  
   let solvedProblems = {};
   
   let levelIndex = {};
@@ -183,174 +182,3 @@ var myChart = new Chart(ctx, {
 .catch((err)=>{
  console.log(err.message);
 })
-// var ctx = document.getElementById('myChart1').getContext('2d');
-// Chart.defaults.font.size = 18;
-// Chart.defaults.color = '#F1C40F';
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['NEWBIE', 'PUPIL', 'SPECIALIST', 'EXPERT', 'CM', 'MASTER','IM','GM','IGM','LGM'],
-//         datasets: [{
-//             label: '# OF USERS',
-//             data: [60,30,50,10,10,20,10,30,40,10],
-//             backgroundColor: [
-//                 'rgb(128 , 128 , 128 , 0.3)',
-//                 'rgba(58, 235, 52, 0.3)',
-//                 'rgba(31, 242, 235, 0.3)',
-//                 'rgba(14, 44, 240, 0.3)',
-//                 'rgba(245, 37, 221, 0.3)',
-//                 'rgba(245, 227, 37, 0.3)',
-//                 'rgba(245, 190, 37, 0.7)',
-//                 'rgba(232, 43, 26, 0.2)',
-//                 'rgba(232, 43, 26, 0.4)',
-//                 'rgba(191, 26, 11, 0.7)'
-//             ],
-//             borderColor: [
-//             'rgb(128 , 128 , 128 , 0.6)',
-//             'rgba(58, 235, 52, 0.6)',
-//             'rgba(31, 242, 235, 0.6)',
-//             'rgba(14, 44, 240, 0.6)',
-//             'rgba(245, 37, 221, 0.6)',
-//             'rgba(245, 227, 37, 0.6)',
-//             'rgba(245, 190, 37, 1)',
-//             'rgba(232, 43, 26, 0.5)',
-//             'rgba(232, 43, 26, 0.7)',
-//             'rgba(191, 26, 11, 1)'
-//             ],
-//             borderWidth: 1,
-//             borderRadius: 4,
-            
-//         }]
-//     },
-//     options: {
-        
-//         plugins: {
-//             title: {
-//                 display: true,
-//                 text: 'NUMBER OF USERS IN EACH LEVEL',
-//                 color:'#F1C40F'
-//             },
-//             legend: {
-//                 display: true,
-//                 labels: {
-//                     color: '#F1C40F'
-//                 }
-//             }
-//         }
-//     }
-// });
-    
-// var ctx = document.getElementById('myChart2').getContext('2d');
-// Chart.defaults.font.size = 16;
-// Chart.defaults.color = '#F1C40F';
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['NEWBIE', 'PUPIL', 'SPECIALIST', 'EXPERT', 'CM', 'MASTER','IM','GM','IGM','LGM'],
-//         datasets: [{
-//             label: '# OF PROBLEMS',
-//             data: [60,30,50,10,10,20,10,30,40,10],
-//             backgroundColor: [
-//                 'rgb(128 , 128 , 128 , 0.3)',
-//                 'rgba(58, 235, 52, 0.3)',
-//                 'rgba(31, 242, 235, 0.3)',
-//                 'rgba(14, 44, 240, 0.3)',
-//                 'rgba(245, 37, 221, 0.3)',
-//                 'rgba(245, 227, 37, 0.3)',
-//                 'rgba(245, 190, 37, 0.7)',
-//                 'rgba(232, 43, 26, 0.2)',
-//                 'rgba(232, 43, 26, 0.4)',
-//                 'rgba(191, 26, 11, 0.7)'
-//             ],
-//             borderColor: [
-//             'rgb(128 , 128 , 128 , 0.6)',
-//             'rgba(58, 235, 52, 0.6)',
-//             'rgba(31, 242, 235, 0.6)',
-//             'rgba(14, 44, 240, 0.6)',
-//             'rgba(245, 37, 221, 0.6)',
-//             'rgba(245, 227, 37, 0.6)',
-//             'rgba(245, 190, 37, 1)',
-//             'rgba(232, 43, 26, 0.5)',
-//             'rgba(232, 43, 26, 0.7)',
-//             'rgba(191, 26, 11, 1)'
-//             ],
-//             borderWidth: 1,
-//             borderRadius: 4,
-            
-//         }]
-//     },
-//     options: {
-        
-//         plugins: {
-//             title: {
-//                 display: true,
-//                 text: 'AVG NUMBER OF PROBLEM SOLVED',
-//                 color:'#F1C40F'
-//             },
-//             legend: {
-//                 display: true,
-//                 labels: {
-//                     color: '#F1C40F'
-//                 }
-//             }
-//         }
-//     }
-// });
-
-// var ctx = document.getElementById('myChart3').getContext('2d');
-// Chart.defaults.font.size = 16;
-// Chart.defaults.color = '#F1C40F';
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['NEWBIE', 'PUPIL', 'SPECIALIST', 'EXPERT', 'CM', 'MASTER','IM','GM','IGM','LGM'],
-//         datasets: [{
-//             label: '# OF CONTEST',
-//             data: [60,30,50,10,10,20,10,30,40,10],
-//             backgroundColor: [
-//                 'rgb(128 , 128 , 128 , 0.3)',
-//                 'rgba(58, 235, 52, 0.3)',
-//                 'rgba(31, 242, 235, 0.3)',
-//                 'rgba(14, 44, 240, 0.3)',
-//                 'rgba(245, 37, 221, 0.3)',
-//                 'rgba(245, 227, 37, 0.3)',
-//                 'rgba(245, 190, 37, 0.7)',
-//                 'rgba(232, 43, 26, 0.2)',
-//                 'rgba(232, 43, 26, 0.4)',
-//                 'rgba(191, 26, 11, 0.7)'
-//             ],
-//             borderColor: [
-//             'rgb(128 , 128 , 128 , 0.6)',
-//             'rgba(58, 235, 52, 0.6)',
-//             'rgba(31, 242, 235, 0.6)',
-//             'rgba(14, 44, 240, 0.6)',
-//             'rgba(245, 37, 221, 0.6)',
-//             'rgba(245, 227, 37, 0.6)',
-//             'rgba(245, 190, 37, 1)',
-//             'rgba(232, 43, 26, 0.5)',
-//             'rgba(232, 43, 26, 0.7)',
-//             'rgba(191, 26, 11, 1)'
-//             ],
-//             borderWidth: 1,
-//             borderRadius: 4,
-            
-//         }]
-//     },
-//     options: {
-        
-//         plugins: {
-//             title: {
-//                 display: true,
-//                 text: 'AVG NUMBER OF CONTEST GIVEN',
-//                 color:'#F1C40F'
-//             },
-//             legend: {
-//                 display: true,
-//                 labels: {
-//                     color: '#F1C40F'
-//                 }
-//             }
-//         }
-//     }
-// });
-
--- routes/visualizerRoutes.js
+++ routes/visualizerRoutes.js
@@ -5,23 +5,33 @@ router.get('/visualizer',(req,res)=>{
    res.render('visualizer',{user:req.user}); 
 });
 
-
-
 router.get('/visualizer/contest/:contestno',async (req,res)=>{
+
    let contestNo = Number(req.params.contestno);
    let uri = "https://codeforces.com/api/contest.standings?contestId=" + contestNo + "&from=1&count=1";
    let response =  await fetch(uri);
    let data = await response.json();
+
    if(data.status != "OK") {
       res.redirect('/error');
    } else {
       let contestID = data.result.contest.id;
       let contestName = data.result.contest.name;
+      let tempName = "";
+      for(let i=0;i<contestName.length;i++) {
+         if(contestName[i]=='(') {
+            break;
+         } else {
+            tempName += contestName[i];
+         }
+      }
+      contestName = tempName;
       let problems = [];
       for(let i=0;i<data.result.problems.length;i++) {
          problems.push(data.result.problems[i].index);
       }
-      res.render('contestv',{user:req.user,contestName:contestName,contestID:contestID,problems:problems});
+
+      res.render('contestVisualizer',{user:req.user,contestName:contestName,contestID:contestID,problems:problems});
    }
 });
 
